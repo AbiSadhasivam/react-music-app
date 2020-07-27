848f164eedd6766840f99f7c8ef0fd8280914738
Abinaya Sadhasivam <abinaya.sadhasivam@solitontech.local>
2020-07-28 02:32:17 +0530

    feat: add loader for fetching data
    Signed-off-by: Abinaya Sadhasivam <abinaya.sadhasivam@solitontech.local>

--- src/components/MusicApp/MusicApp.js
+++ src/components/MusicApp/MusicApp.js
@@ -1,23 +1,33 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { apiEndpoints } from '../../constants/apiEndpoints';
 import { apiFetch } from '../../services/apiService/apiService';
 import { useDispatch } from 'react-redux';
 import * as storeAction from '../../store/actions/action';
 import Header from '../Header/Header';
 import Body from '../Body/Body';
+import Loader from '../Loader/Loader';
 const MusicApp = () => {
   const dispatch = useDispatch();
+  const [showLoader, setLoaderVisiblity] = useState(false);
 
+  // Visiblity
+
+  let toggleLoader = (isVisible) => {
+    setLoaderVisiblity(isVisible);
+  };
   const fetMusicLibrary = () => {
+    toggleLoader(true);
     apiFetch(apiEndpoints.albums, 'GET').then(
       (data) => {
         dispatch(storeAction.createAlbumList(data));
         apiFetch(apiEndpoints.songs, 'GET').then(
           (data) => {
             dispatch(storeAction.createSongList(data));
+            toggleLoader(false);
           },
           (err) => {
             console.log(err);
+            toggleLoader(false);
             // TODO : Show loader and show error notification
           }
         );
@@ -35,6 +45,7 @@ const MusicApp = () => {
 
   return (
     <div>
+      {showLoader && <Loader></Loader>}
       <Header></Header>
       <Body></Body>
     </div>
